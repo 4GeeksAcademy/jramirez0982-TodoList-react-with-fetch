985f6e05a2009edecdb611ac56f7cf808e4e9f18
Julian Andres Ramirez <116443557+jramirez0982@users.noreply.github.com>
2025-05-15 13:30:42 +0000

    Creacion de To do List

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import ListElement from "./ListElement";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	let arrayTask = [];
+	const [taskList, setTaskList] = useState([]);
+	const [deleteTask, setDeleteTask] = useState(-1);
+
+	const handleKeyDown = (event) => {
+		if (event.keyCode === 13) {
+			let copyTasks = [...taskList]
+			copyTasks.push(event.target.value)
+			setTaskList(copyTasks)
+			console.log(taskList)
+			event.target.value = "";
+		};
+
+	}
+
+	return (
+		<div className="text-center container">
+
+			<h1 className="text-center mt-5">TO DO LIST!</h1>
+			<input className="form-control" placeholder="Ingrese una nueva tarea"
+				onKeyDown={handleKeyDown} />
+
+			{
+				taskList.map((task, index) => {
+					return (<div className="position-relative mt-2">
+						<p className="form-control text-start" rows="1">{task}</p>
+						<button id={index} type="button" className="btn-close btn-close-hover position-absolute top-0 end-0 m-2"
+							onClick={() => {
+								taskList.splice(index, 1)
+								console.log({ index })
+								console.log(taskList)
+								setDeleteTask({ index })
+							}
+							} />
+					</div>)
+
+				})
+			}
+
 		</div>
 	);
 };
---
+++ src/js/components/ListElement.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const ListElement = () => {
+  return (
+    <div class="position-relative mt-2">
+				<p className="form-control" rows="1"/>
+				<button type="button" className="btn-close position-absolute top-0 end-0 m-2"/>
+		</div>
+  )
+}
+
+export default ListElement
