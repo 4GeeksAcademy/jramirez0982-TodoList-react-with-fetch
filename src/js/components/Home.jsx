a098134dcfae96e519d3fbcd2cd244f0d19fa48f
Julian Andres Ramirez <116443557+jramirez0982@users.noreply.github.com>
2025-05-22 02:50:50 +0000

    [CREACION DE FUNCIONES CON METODOS POST, GET Y DEL] home.jsx: se modifico el codigo incuyendo funciones para crear y eliminar tareas una a una o todas de una vez

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,20 +1,79 @@
-import React, { useState } from "react";
+import { Button } from "bootstrap";
+import React, { useState, useEffect } from "react";
+
 
 //include images into your bundle
 
 
 //create your first component
 const Home = () => {
+	useEffect(() => {
+		getTaskList()
+	}, [])
 
-	let arrayTask = [];
+
+	//let arrayTask = [];
 	const [taskList, setTaskList] = useState([]);
+
 
 	const handleKeyDown = (event) => {
 		if (event.keyCode === 13) {
-			setTaskList([...taskList, event.target.value])
+
+			fetch("https://playground.4geeks.com/todo/todos/julian_ramirezr", {
+				method: "POST",
+				body: JSON.stringify({ label: event.target.value, is_done: false }),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			})
+				.then((response) => {
+					return response.json()
+				})
+				.then((data) => {
+					console.log(data)
+					getTaskList()
+				})
+				.catch((err) => { err })
 			event.target.value = "";
 		};
+	}
 
+	function getTaskList() {
+		fetch("https://playground.4geeks.com/todo/users/julian_ramirezr")
+			.then((response) => {
+				console.log(response)
+				if (response.ok == false) {
+					throw new Error("Error al traer los datos")
+				}
+				return response.json()
+			})
+			.then((data) => {
+				console.log(data.todos)
+				setTaskList(data.todos)
+			})
+			.catch((error) => {
+				alert(Error)
+			})
+	}
+
+	function deleteTask(indexTaskToDelete) {
+		let idTask = taskList[indexTaskToDelete].id
+		console.log(idTask)
+		let urlofTask = "https://playground.4geeks.com/todo/todos/" + idTask.toString()
+		console.log(urlofTask)
+
+		fetch(urlofTask, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then((response) => {
+				if (response.ok)
+					console.log("informacion eliminada")
+				getTaskList()
+			})
+			.catch((err) => { err })
 	}
 
 	return (
@@ -27,20 +86,31 @@ const Home = () => {
 			{
 				taskList.map((task, index) => {
 					return (<div className="position-relative mt-2">
-						<p className="form-control text-start mb-0" rows="1">{task}</p>
+						<p className="form-control text-start mb-0" rows="1">{task.label}</p>
 						<button id={index} type="button" className="btn-close btn-close-hover position-absolute top-0 end-0 m-2 mb-0"
 							onClick={() => {
-								setTaskList(taskList.filter((value, indexTaskList) => {
-									return index !== indexTaskList
-								}))
+								deleteTask(index)
 							}
 							} />
 					</div>)
+
 				})
 			}
 			<small className="text-muted d-flex text-star mx-2">
 				{taskList.length == 0 ? "No hay pendientes, añadir tareas" : taskList.length + " item left"}
 			</small>
+			<div className="container d-flex flex-column align-items-center m-3">
+				
+				<button className="btn btn-primary col-4 m-1" onClick={
+					getTaskList}>Mostrar tareas</button>
+				
+				<button className="btn btn-danger col-4 m-1" onClick={() => {
+					for (let i = 0; i < taskList.length; i++) {
+						deleteTask(i);
+					}
+					getTaskList()
+				}} >Borrar todo</button>
+			</div>
 		</div>
 	);
 };
